c69af31f8ff0ec132f5eff4dbd86990f57f5da6d
ItsFlicker <1812496278@qq.com>
2021-10-03 00:12:14 +0800

    + [1.90-R2] Fix cloud chat filter (again)

--- src/main/kotlin/me/arasple/mc/trchat/common/filter/ChatFilter.kt
+++ src/main/kotlin/me/arasple/mc/trchat/common/filter/ChatFilter.kt
@@ -23,13 +23,13 @@ import java.nio.charset.StandardCharsets
 @PlatformSide([Platform.BUKKIT])
 object ChatFilter {
 
-    private var CLOUD_LAST_UPDATE: String? = null
-    private val CLOUD_URL = mutableListOf<String>()
-    private var CLOUD_LAST_WORDS = listOf<String>()
+    private val CLOUD_LAST_UPDATE = mutableMapOf<String, String>()
+    private var CLOUD_URL = mutableListOf<String>()
+    private val CLOUD_WORDS = mutableSetOf<String>()
 
-    @Schedule(delay = 20 * 120, period = 30 * 60 * 20, async = true)
+    @Schedule(delay = (20 * 120).toLong(), period = (30 * 60 * 20).toLong(), async = true)
     fun asyncRefreshCloud() {
-        loadCloudFilter(0)
+        loadCloudFilter()
     }
 
     /**
@@ -46,51 +46,61 @@ object ChatFilter {
 
         // 更新云端词库
         if (updateCloud && filter.getBoolean("CLOUD-THESAURUS.ENABLE", false)) {
-            CLOUD_URL.clear()
-            CLOUD_URL += filter.getStringList("CLOUD-THESAURUS.URL")
+            CLOUD_URL = filter.getStringList("CLOUD-THESAURUS.URL")
             submit(async = true) {
-                Filter.addSensitiveWord(loadCloudFilter(0, *notify).let {
-                    if (it.isEmpty()) {
-                        CLOUD_LAST_WORDS
-                    } else {
-                        CLOUD_LAST_WORDS = it
-                        it
-                    }
-                })
+                loadCloudFilter(*notify)
             }
-        } else {
-            notify(notify, "Plugin-Loaded-Filter-Local", filter.getStringList("LOCAL").size)
         }
+        notify(notify, "Plugin-Loaded-Filter-Local", filter.getStringList("LOCAL").size)
     }
 
     /**
      * 加载云端聊天敏感词库
      *
+     * @param notify 接受通知反馈
+     */
+    private fun loadCloudFilter(vararg notify: ProxyCommandSender) {
+        if (CLOUD_URL.isEmpty()) {
+            return
+        }
+        val collected = mutableListOf<String>()
+        for (i in CLOUD_URL.indices) {
+            collected += catchCloudThesaurus(i, *notify)
+        }
+        CLOUD_WORDS += collected
+        if (CLOUD_WORDS.isEmpty()) {
+            notify(notify, "Plugin-Failed-Load-Filter-Cloud")
+        } else {
+            Filter.addSensitiveWord(CLOUD_WORDS.toList())
+        }
+    }
+
+    /**
+     * 抓取云端聊天敏感词库
+     *
      * @param url    尝试 URL 序号
      * @param notify 接受通知反馈
      */
-    private fun loadCloudFilter(url: Int, vararg notify: ProxyCommandSender): List<String> {
-        if (CLOUD_URL.isEmpty()) {
-            return emptyList()
-        }
+    private fun catchCloudThesaurus(url: Int, vararg notify: ProxyCommandSender): List<String> {
+        val address = CLOUD_URL[url]
         val whitelist = filter.getStringList("CLOUD-THESAURUS.WHITELIST")
-        val collected = mutableSetOf<String>()
+        val collected = mutableListOf<String>()
 
         return kotlin.runCatching {
-            URL(CLOUD_URL[url]).openStream().use { inputStream ->
+            URL(address).openStream().use { inputStream ->
                 BufferedInputStream(inputStream).use { bufferedInputStream ->
                     val database = JsonParser().parse(readFully(bufferedInputStream, StandardCharsets.UTF_8)).asJsonObject
                     if (!database.has("lastUpdateDate") || !database.has("words")) {
                         error("Wrong database json object")
                     }
 
-                    val lastUpdateDate = database.get("lastUpdateDate").asString
-                    CLOUD_LAST_UPDATE = when (CLOUD_LAST_UPDATE) {
+                    val lastUpdateDate = database["lastUpdateDate"].asString
+                    CLOUD_LAST_UPDATE[address] = when (CLOUD_LAST_UPDATE[address]) {
                         null -> lastUpdateDate
                         lastUpdateDate -> return emptyList()
                         else -> lastUpdateDate
                     }
-                    database.get("words").asJsonArray.forEach {
+                    database["words"].asJsonArray.forEach {
                         val word = it.asString
                         if (whitelist.none { w -> w.equals(word, ignoreCase = true) }) {
                             collected.add(word)
@@ -98,21 +108,11 @@ object ChatFilter {
                     }
                 }
             }
-            if ((url + 1) < CLOUD_URL.size) {
-                collected += loadCloudFilter(url + 1)
-            }
-            collected.toList()
+            notify(notify, "Plugin-Loaded-Filter-Cloud", collected.size, url, CLOUD_LAST_UPDATE[address]!!)
+            collected
         }.getOrElse {
             it.printStackTrace()
             emptyList()
-        }.also {
-            if (it.isNotEmpty()) {
-                notify(notify, "Plugin-Loaded-Filter-Local", filter.getStringList("LOCAL").size)
-                notify(notify, "Plugin-Loaded-Filter-Cloud", collected.size, CLOUD_URL.size, CLOUD_LAST_UPDATE!!)
-            } else {
-                notify(notify, "Plugin-Loaded-Filter-Local", filter.getStringList("LOCAL").size)
-                notify(notify, "Plugin-Failed-Load-Filter-Cloud")
-            }
         }
     }
 
