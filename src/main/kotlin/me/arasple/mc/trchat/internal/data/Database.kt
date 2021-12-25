b0ab9673827eee859d6c87e788f8e7404d6648f9
ItsFlicker <1812496278@qq.com>
2021-12-25 23:01:40 +0800

    + [1.90-R5] Try to fix database

--- src/main/kotlin/me/arasple/mc/trchat/internal/data/Database.kt
+++ src/main/kotlin/me/arasple/mc/trchat/internal/data/Database.kt
@@ -1,7 +1,6 @@
 package me.arasple.mc.trchat.internal.data
 
 import me.arasple.mc.trchat.api.TrChatFiles.settings
-import me.arasple.mc.trchat.internal.database.DatabaseLocal
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import taboolib.common.io.newFile
@@ -9,7 +8,7 @@ import taboolib.common.platform.Platform
 import taboolib.common.platform.PlatformSide
 import taboolib.common.platform.event.SubscribeEvent
 import taboolib.common.platform.function.getDataFolder
-import taboolib.expansion.getDataContainer
+import taboolib.common.platform.function.submit
 import taboolib.expansion.releaseDataContainer
 import taboolib.expansion.setupDataContainer
 import taboolib.expansion.setupPlayerDatabase
@@ -25,31 +24,20 @@ import java.io.File
 @PlatformSide([Platform.BUKKIT])
 object Database {
 
-    var old_database: DatabaseLocal? = null
-
     fun init() {
         if (settings.getBoolean("database.enable")) {
             setupPlayerDatabase(settings.getConfigurationSection("database")!!)
         } else {
             setupPlayerDatabase(newFile(File(getDataFolder(), "data"), "data.db"))
         }
-        if (File(getDataFolder(), "data.db").exists()) {
-            old_database = DatabaseLocal()
-        }
     }
 
     @SubscribeEvent
     fun e(e: PlayerJoinEvent) {
-        e.player.setupDataContainer()
-        // Migrate
-        old_database?.let { database ->
-            database.pull(e.player)?.run {
-                e.player.getDataContainer()["filter"] = getBoolean("FILTER", false)
-                e.player.getDataContainer()["mute_time"] = getLong("MUTE_TIME", 0)
-                e.player.getDataContainer()["custom_channel"] = getString("CUSTOM-CHANNEL", "")!!
-            }
-            database.release(e.player)
+        submit(async = true) {
+            e.player.setupDataContainer()
         }
+
     }
 
     @SubscribeEvent
